108346193de72e90b52e99d711bcfdf080c94000
Zeynep Kardelen Soğukoğlu <64661378+zeysoguk@users.noreply.github.com>
2021-02-06 01:54:01 +0300

    Add files via upload

---
+++ hesapmakinesi.py
@@ -0,0 +1,33 @@
+print("""************************************
+Hesap Makinesi
+
+İşlemler:
+
+1. Toplama İşlemi
+
+2. Çıkarma İşlemi
+
+3. Çarpma İşlemi
+
+4. Bölme İşlemi
+************************************""")
+
+a = int(input("Birinci Sayı:"))
+b = int(input("İkinci Sayı:"))
+
+işlem = input("İşlemi Giriniz:")
+
+if işlem == "1":
+    print("{} ile {} toplamı {}".format(a, b, a + b))
+
+elif işlem == "2":
+    print("{} ile {} farkı {}".format(a, b, a - b))
+
+elif işlem == "3":
+    print("{} ile {} çarpımı {}".format(a, b, a * b))
+
+elif işlem == "4":
+    print("{} ile {} bölümü {}".format(a, b, a / b))
+
+else:
+    print("Geçersiz İşlem!")
